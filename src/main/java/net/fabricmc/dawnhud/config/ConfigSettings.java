a7eae552c0ce7b5d6af45f6245af1b4a249afe1f
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-01-10 13:47:24 -0500

    Saved Module Positions

--- src/main/java/net/fabricmc/dawnhud/config/ConfigSettings.java
+++ src/main/java/net/fabricmc/dawnhud/config/ConfigSettings.java
@@ -8,4 +8,12 @@ public class ConfigSettings {
 
     @SerializedName("enable_coords")
     public Boolean EnableCoords;
+
+    @SerializedName("fps_position")
+    public int xFpsPos;
+    public int yFpsPos;
+
+    @SerializedName("coords_position")
+    public int xCoordsPos;
+    public int yCoordsPos;
 }
