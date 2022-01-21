d955cd8d0184bb2f3f922be59ad4936686b5bca9
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-01-20 22:32:14 -0500

    Added Time Module

--- src/main/java/net/fabricmc/dawnhud/gui/ConfigGUI.java
+++ src/main/java/net/fabricmc/dawnhud/gui/ConfigGUI.java
@@ -1,49 +1,144 @@
 package net.fabricmc.dawnhud.gui;
 
-import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
-import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
-import io.github.cottonmc.cotton.gui.widget.WToggleButton;
+import io.github.cottonmc.cotton.gui.widget.*;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.dawnhud.DawnClient;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
+
+import javax.swing.*;
 
 @Environment(EnvType.CLIENT)
 public class ConfigGUI extends LightweightGuiDescription  {
+    /* Root Panel */
+    Text basicTabText = new TranslatableText("gui.dawnhud.basic_options");
+    Text advancedTabText = new TranslatableText("gui.dawnhud.advanced_options");
+
+    /* Panel A */
+    static final Text fpsToggleButtonText = new TranslatableText("dawnhud.config.basic.enable_fps");
+    static final Text coordsToggleButtonText = new TranslatableText("dawnhud.config.basic.enable_coords");
+    static final Text worldTimeToggleButtonText = new TranslatableText("dawnhud.config.basic.enable_time");
+
+    /* Panel B */
+    static Text positionHeaderText = new TranslatableText("gui.dawn.config.category.position");
+    static Text colorHeaderText = new TranslatableText("gui.dawn.config.category.color");
+    static final Text timeHeaderText = new TranslatableText("gui.dawn.config.category.time");
+
+    // static final Text hours12Text = new TranslatableText("dawnhud.config.advanced.12hour");
+    // static final Text hours24Text = new TranslatableText("dawnhud.config.advanced.24hour");
+
     public ConfigGUI() {
         WPlainPanel root = new WPlainPanel();
         setRootPanel(root);
-        root.setSize(100, 50);
+        root.setSize(300, 200);
         root.setInsets(Insets.ROOT_PANEL);
-        root.setBackgroundPainter(BackgroundPainter.VANILLA);
         root.setHost(this);
+        addPainters();
 
-        WToggleButton fpsToggleButton = new WToggleButton(new TranslatableText("dawnhud.config.enable_fps"));
-        WToggleButton coordsToggleButton = new WToggleButton(new TranslatableText("dawnhud.config.enable_coords"));
+        WTabPanel tabs = new WTabPanel();
+        root.add(tabs, 0, 0, root.getWidth(), root.getHeight() - 20);
 
-        if (DawnClient.getInstance().config.EnableFPS)
-            fpsToggleButton.setToggle(true);
-        else
-            fpsToggleButton.setToggle(false);
+        WPlainPanel panelA = new WPlainPanel();
+        WPlainPanel panelB = new WPlainPanel();
+
+        tabs.add(panelA, tab -> tab.title(basicTabText));
+        tabs.add(panelB, tab -> tab.title(advancedTabText));
+
+        setPanelA(tabs, panelA);
+        setPanelB(tabs, panelB);
+
+        root.validate(this);
+    }
+
+    private static void setPanelA(WTabPanel tabs, WPlainPanel panelA) {
+        /* Panel Settings */
+        panelA.setSize(tabs.getWidth(), tabs.getHeight());
+        panelA.setInsets(Insets.ROOT_PANEL);
+
+        /* Module Definition */
+        WToggleButton fpsToggleButton = new WToggleButton(fpsToggleButtonText);
+        WToggleButton coordsToggleButton = new WToggleButton(coordsToggleButtonText);
+        WToggleButton worldTimeToggleButton = new WToggleButton(worldTimeToggleButtonText);
+
+        /* Modules */
+        setFpsToggleButton(fpsToggleButton);
+        setCoordsToggleButton(coordsToggleButton);
+        setWorldTimeToggleButton(worldTimeToggleButton);
+
+        /* Adding them to panel render */
+        panelA.add(fpsToggleButton, 6, 6, panelA.getWidth() - 16, 20);
+        panelA.add(coordsToggleButton, 6, 24, panelA.getWidth() - 16, 20);
+        panelA.add(worldTimeToggleButton, 6, 42, panelA.getWidth() - 16, 20);
+    }
+
+    private static void setPanelB(WTabPanel tabs, WPlainPanel panelB) {
+        /* Panel Settings */
+        panelB.setSize(tabs.getWidth(), tabs.getHeight());
+        panelB.setInsets(Insets.ROOT_PANEL);
+
+        /* Module Definition */
+        WText positionHeader = new WText(positionHeaderText);
+        WText colorHeader = new WText(colorHeaderText);
+        WText timeHeader = new WText(timeHeaderText);
+
+        WButton editPosition = new WButton(new TranslatableText("dawnhud.config.advanced.edit_position"));
+        WButton editColors = new WButton(new TranslatableText("dawnhud.config.advanced.edit_colors"));
+        WButton hours12 = new WButton(new TranslatableText("dawnhud.config.advanced.12hour").append(": ").append(new TranslatableText("dawnhud.on").formatted(Formatting.GREEN)));
+        WButton hours24 = new WButton(new TranslatableText("dawnhud.config.advanced.24hour").append(": ").append(new TranslatableText("dawnhud.off").formatted(Formatting.RED)));
+
+        /* Modules */
+        setClockPeriod(hours12, hours24);
+
+        /* Adding Modules to Panel Render */
+        panelB.add(positionHeader, panelB.getWidth() / 4, 6, panelB.getWidth() / 2, 20);
+        panelB.add(colorHeader, panelB.getWidth() / 4, 46, panelB.getWidth() / 2, 20);
+        panelB.add(timeHeader, panelB.getWidth() / 4, 86, panelB.getWidth() / 2, 20);
+
+        panelB.add(editPosition, panelB.getWidth() / 4, 20, panelB.getWidth() / 2, 20);
+        panelB.add(editColors, panelB.getWidth() / 4, 60, panelB.getWidth() / 2, 20);
+        panelB.add(hours12, 6, 100, panelB.getWidth() / 2 - 6, 20);
+        panelB.add(hours24, panelB.getWidth() / 2 + 6, 100, panelB.getWidth() / 2 - 12, 20);
+
+        editPosition.setAlignment(HorizontalAlignment.CENTER);
+        editColors.setAlignment(HorizontalAlignment.CENTER);
+
+        positionHeader.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        colorHeader.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        timeHeader.setHorizontalAlignment(HorizontalAlignment.CENTER);
+    }
+
+    private static void displayClockPeriod(WButton hours12, WButton hours24) {
+        if (DawnClient.getInstance().config.Enable12Hours) {
+            hours12.setLabel(new TranslatableText("dawnhud.config.advanced.12hour").append(": ").append(new TranslatableText("dawnhud.off").formatted(Formatting.RED)));
+            hours24.setLabel(new TranslatableText("dawnhud.config.advanced.24hour").append(": ").append(new TranslatableText("dawnhud.on").formatted(Formatting.GREEN)));
+        }
+        else {
+            hours12.setLabel(new TranslatableText("dawnhud.config.advanced.12hour").append(": ").append(new TranslatableText("dawnhud.on").formatted(Formatting.GREEN)));
+            hours24.setLabel(new TranslatableText("dawnhud.config.advanced.24hour").append(": ").append(new TranslatableText("dawnhud.off").formatted(Formatting.RED)));
+        }
+    }
+
+    /* saves to config */
+    private static void setFpsToggleButton(WToggleButton fpsToggleButton) {
+        fpsToggleButton.setToggle(DawnClient.getInstance().config.EnableFPS);
 
         fpsToggleButton.setOnToggle(on -> {
             if (on) {
                 DawnClient.getInstance().config.EnableFPS = true;
                 DawnClient.getInstance().saveConfig();
-            }
-            else {
+            } else {
                 DawnClient.getInstance().config.EnableFPS = false;
                 DawnClient.getInstance().saveConfig();
             }
         });
-
-        if (DawnClient.getInstance().config.EnableCoords)
-            coordsToggleButton.setToggle(true);
-        else
-            coordsToggleButton.setToggle(false);
+    }
+    private static void setCoordsToggleButton(WToggleButton coordsToggleButton) {
+        coordsToggleButton.setToggle(DawnClient.getInstance().config.EnableCoords);
 
         coordsToggleButton.setOnToggle(on -> {
             if (on) {
@@ -55,10 +150,58 @@ public class ConfigGUI extends LightweightGuiDescription  {
                 DawnClient.getInstance().saveConfig();
             }
         });
-
-        root.add(fpsToggleButton, 20, 5, 200, 20);
-        root.add(coordsToggleButton, 20, 25, 200, 20);
-
-        root.validate(this);
     }
+    private static void setWorldTimeToggleButton(WToggleButton worldTimeToggleButton) {
+        worldTimeToggleButton.setToggle(DawnClient.getInstance().config.EnableTime);
+
+        worldTimeToggleButton.setOnToggle(on -> {
+            if (on) {
+                DawnClient.getInstance().config.EnableTime = true;
+                DawnClient.getInstance().saveConfig();
+            }
+            else {
+                DawnClient.getInstance().config.EnableTime = false;
+                DawnClient.getInstance().saveConfig();
+            }
+        });
+    }
+    private static void setClockPeriod(WButton hours12, WButton hours24) {
+        hours12.setOnClick(() -> {
+            if (DawnClient.getInstance().config.Enable12Hours) {
+                displayClockPeriod(hours12, hours24);
+                DawnClient.getInstance().config.Enable24Hours = true;
+                DawnClient.getInstance().config.Enable12Hours = false;
+            }
+            else {
+                displayClockPeriod(hours12, hours24);
+                DawnClient.getInstance().config.Enable24Hours = false;
+                DawnClient.getInstance().config.Enable12Hours = true;
+            }
+
+            DawnClient.getInstance().saveConfig();
+        });
+        hours24.setOnClick(() -> {
+            if (DawnClient.getInstance().config.Enable24Hours) {
+                displayClockPeriod(hours12, hours24);
+                DawnClient.getInstance().config.Enable24Hours = false;
+                DawnClient.getInstance().config.Enable12Hours = true;
+            }
+            else {
+                displayClockPeriod(hours12, hours24);
+                DawnClient.getInstance().config.Enable24Hours = true;
+                DawnClient.getInstance().config.Enable12Hours = false;
+            }
+
+
+            DawnClient.getInstance().saveConfig();
+        });
+    }
+
+
+    /**
+    * Allows the Overriding of the GuiDescription.addPainters method which adds a default background to any null backgroundPainter calls.
+    * To quote Juuz, "I need to make it not automatically apply to tab panels" bruh
+    */
+    @Override
+    public void addPainters() {}
 }
--- src/main/java/net/fabricmc/dawnhud/mixin/DisplayRenderMixin.java
+++ src/main/java/net/fabricmc/dawnhud/mixin/DisplayRenderMixin.java
@@ -1,6 +1,8 @@
 package net.fabricmc.dawnhud.mixin;
 
+import io.github.cottonmc.cotton.gui.widget.WButton;
 import net.fabricmc.dawnhud.DawnClient;
+import net.fabricmc.dawnhud.gui.ConfigGUI;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
@@ -9,47 +11,80 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Mixin(InGameHud.class)
 public class DisplayRenderMixin {
 	@Inject(at = @At("TAIL"), method = "render")
 	private void render(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
 		MinecraftClient client = MinecraftClient.getInstance();
 
+		if (!client.options.debugEnabled) {
+			if (DawnClient.getInstance().config.EnableFPS) // Checks if enabled in Config
+				displayFPS(client, matrixStack);
+
+			if (DawnClient.getInstance().config.EnableCoords)
+				displayCoords(client, matrixStack);
+
+			if (DawnClient.getInstance().config.EnableTime)
+				displayTime(client, matrixStack);
+		}
+	}
+
+	/* Module Renderers */
+	private void displayFPS(MinecraftClient client, MatrixStack matrixStack) {
 		int currentFPS;
+		String fpsDisplayString;
+
+		currentFPS = getFPS(client);
+		fpsDisplayString = currentFPS + " FPS";
+
+		if (currentFPS <= 60)
+			client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xFF0000);
+		else if (currentFPS < 100 && currentFPS > 60)
+			client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xE7E700);
+		else if (currentFPS >= 100)
+			client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xFFFFFF);
+	}
+	private void displayCoords(MinecraftClient client, MatrixStack matrixStack) {
 		int currentX;
 		int currentY;
 		int currentZ;
 
-		String fpsDisplayString;
 		String coordDisplayString;
 
-		if (!client.options.debugEnabled) {
-			if (!DawnClient.getInstance().config.EnableFPS) {} // Checks if enabled in Config
-			else {
-				currentFPS = getFPS(client);
-				fpsDisplayString = currentFPS + " FPS";
+		currentX = getX(client);
+		currentY = getY(client);
+		currentZ = getZ(client);
 
-				if (currentFPS <= 60)
-					client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xFF0000);
-				else if (currentFPS < 100 && currentFPS > 60)
-					client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xE7E700);
-				else if (currentFPS >= 100)
-					client.textRenderer.drawWithShadow(matrixStack, fpsDisplayString, 5, 10, 0xFFFFFF);
-			}
+		coordDisplayString = currentX + ", " + currentY + ", " + currentZ;
 
-			if (!DawnClient.getInstance().config.EnableCoords) {} // Checks if enabled in Config
-			else {
-				currentX = getX(client);
-				currentY = getY(client);
-				currentZ = getZ(client);
+		client.textRenderer.drawWithShadow(matrixStack, coordDisplayString, 5, 20, 0xFFFFFF);
+	}
+	private void displayTime(MinecraftClient client, MatrixStack matrixStack) {
+		long worldTime;
+		int hours;
+		int minutes;
+		String clock;
 
-				coordDisplayString = currentX + ", " + currentY + ", " + currentZ;
+		worldTime = (client.player.world.getTimeOfDay() + 6000);
+		hours = (int) (worldTime / 1000) % 24;
+		minutes = (int) ((worldTime % 1000) * 60 / 1000);
 
-				client.textRenderer.drawWithShadow(matrixStack, coordDisplayString, 5, 20, 0xFFFFFF);
-			}
+		if (DawnClient.getInstance().config.Enable12Hours) {
+			if (hours < 1)
+				clock = (12 + ":" + (minutes < 10 ? ("0" + minutes) : minutes) + " " + ((hours >= 12) ? "PM" : "AM"));
+			else
+				clock = (hours > 12 ? ("0" + hours % 12) : hours) + ":" + (minutes < 10 ? ("0" + minutes) : minutes) + " " + ((hours >= 12) ? "PM" : "AM");
 		}
+		else
+			clock = hours + ":" + (minutes < 10 ? ("0" + minutes) : minutes);
+
+		client.textRenderer.drawWithShadow(matrixStack, clock, 5, 30, 0xFFFFFF);
 	}
 
+	/* Specific method calls to the client */
 	private int getFPS(MinecraftClient client) { return ((FPSDisplayMixin) client).getCurrentFPS(); }
 
 	private int getX(MinecraftClient client) { return (int)((client).player.getX()); }
