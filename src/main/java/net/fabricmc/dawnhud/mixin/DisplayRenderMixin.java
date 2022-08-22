6205f9fe30095b73f978f7f1f844a5e66463b0eb
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-08-22 17:54:27 -0400

    extracted module renderers

--- src/main/java/net/fabricmc/dawnhud/mixin/DisplayRenderMixin.java
+++ src/main/java/net/fabricmc/dawnhud/mixin/DisplayRenderMixin.java
@@ -1,10 +1,13 @@
 package net.fabricmc.dawnhud.mixin;
 
 import net.fabricmc.dawnhud.DawnClient;
-import net.fabricmc.dawnhud.hud.DisplayHud;
+import net.fabricmc.dawnhud.modules.Coordinates;
+import net.fabricmc.dawnhud.modules.Fps;
+import net.fabricmc.dawnhud.modules.Time;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,18 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class DisplayRenderMixin {
-	int currentX;
-	int currentY;
-	int currentZ;
+	private MatrixStack matrixStack;
 
 	@Inject(at = @At("TAIL"), method = "render")
 	private void render(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
 		MinecraftClient client = MinecraftClient.getInstance();
+		this.matrixStack = matrixStack;
 
-		new DisplayHud(matrixStack, client);
-
-		if (!client.options.debugEnabled) {
-			if (DawnClient.getInstance().config.EnableFPS) // Checks if enabled in Config
+		if (!client.options.debugEnabled) { // Checks if enabled in Config
+			if (DawnClient.getInstance().config.EnableFPS)
 				renderFPS(client);
 			if (DawnClient.getInstance().config.EnableCoords)
 				renderCoords(client);
@@ -33,20 +33,7 @@ public class DisplayRenderMixin {
 	}
 
 	/* Module Renderers */
-	private void renderFPS(MinecraftClient client) { DisplayHud.displayFPS(getFPS(client)); }
-	private void renderTime(MinecraftClient client) { DisplayHud.displayTime((client.player.world.getTimeOfDay() + 6000)); }
-	private void renderCoords(MinecraftClient client) {
-		currentX = getX(client);
-		currentY = getY(client);
-		currentZ = getZ(client);
-
-		DisplayHud.displayCoords(currentX + ", " + currentY + ", " + currentZ);
-	}
-
-	/* Method calls to the client */
-	private int getFPS(MinecraftClient client) { return ((FPSDisplayMixin) client).getCurrentFPS(); }
-
-	private int getX(MinecraftClient client) { return (int)((client).player.getX()); }
-	private int getY(MinecraftClient client) { return (int)((client).player.getY()); }
-	private int getZ(MinecraftClient client) { return (int)((client).player.getZ()); }
+	private void renderFPS(MinecraftClient client) { new Fps().renderFps(client, matrixStack); }
+	private void renderTime(MinecraftClient client) { new Time().renderTime(client, matrixStack); }
+	private void renderCoords(MinecraftClient client) { new Coordinates().renderCoordinates(client, matrixStack); }
 }
