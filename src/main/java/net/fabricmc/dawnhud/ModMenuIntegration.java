d57ede99ba412544f8191940719aa202c5c37563
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-08-22 17:55:07 -0400

    package refactor

--- src/main/java/net/fabricmc/dawnhud/ModMenuIntegration.java
+++ src/main/java/net/fabricmc/dawnhud/ModMenuIntegration.java
@@ -4,7 +4,7 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.dawnhud.Screens.DawnSettingsScreen;
+import net.fabricmc.dawnhud.screens.DawnSettingsScreen;
 import net.fabricmc.dawnhud.gui.ConfigGUI;
 
 @Environment(EnvType.CLIENT)
