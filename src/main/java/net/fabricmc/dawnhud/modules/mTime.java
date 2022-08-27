c18b8657c037dba4648910dc7b4ab2255837e683
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-08-26 22:26:55 -0400

    fixed incorrect time display
    this is incredibly unneeded and will be reverted in the future lol

--- src/main/java/net/fabricmc/dawnhud/modules/mTime.java
+++ src/main/java/net/fabricmc/dawnhud/modules/mTime.java
@@ -10,16 +10,27 @@ public class mTime {
 
         int hours;
         int minutes;
-        String clock;
+        String clock = "";
 
         hours = (int) (worldTime / 1000) % 24;
         minutes = (int) ((worldTime % 1000) * 60 / 1000);
 
-        if (DawnClient.getInstance().config.Enable12Hours) {
-            if (hours < 1)
-                clock = 12 + ":" + (minutes < 10 ? "0" + minutes : minutes) + " " + "AM";
-            else
-                clock = (hours > 12 ? ("0" + hours % 12) : hours) + ":" + (minutes < 10 ? ("0" + minutes) : minutes) + " " + ((hours >= 12) ? "PM" : "AM");
+        if (DawnClient.getInstance().config.Enable12Hours) {// Now ik this looks bad, and it is, I'm sorry
+            clock = (hours < 10) ? (hours < 1 ? "12" : "0" + hours) : (hours > 12 && hours < 22) ? "0" + hours % 12 : (hours == 12 ? "12" : String.valueOf(hours % 12));
+            clock += ":";
+            clock += (minutes < 10) ? ("0" + minutes) : (minutes);
+            clock +=  (hours < 12) ? " AM" : " PM";
+
+//            if (minutes < 10) {
+//                clock += "0" + minutes;
+//            }
+//            else
+//                clock += minutes;
+//
+//            if (hours <= 12)
+//                clock += " AM";
+//            else
+//                clock += " PM";
         }
         else
             clock = hours + ":" + (minutes < 10 ? ("0" + minutes) : minutes);
